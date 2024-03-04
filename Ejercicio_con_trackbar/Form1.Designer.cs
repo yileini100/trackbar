aa2cf706f97f3711d046ca5fe82615c3f86666aa
yoga 11e <yoga 11e@DESKTOP-OOOUNS6>
2024-03-04 19:52:22 -0400

    Agregar archivos de proyecto.

---
+++ Ejercicio_con_trackbar/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Ejercicio_con_trackbar
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            trckBar1 = new TrackBar();
+            ((System.ComponentModel.ISupportInitialize)trckBar1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(80, 229);
+            label1.Name = "label1";
+            label1.Size = new Size(68, 30);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // trckBar1
+            // 
+            trckBar1.BackColor = Color.Snow;
+            trckBar1.Location = new Point(200, 123);
+            trckBar1.Margin = new Padding(3, 2, 3, 2);
+            trckBar1.Name = "trckBar1";
+            trckBar1.Size = new Size(363, 45);
+            trckBar1.TabIndex = 1;
+            trckBar1.Scroll += trckBar1_Scroll;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Info;
+            ClientSize = new Size(700, 338);
+            Controls.Add(trckBar1);
+            Controls.Add(label1);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)trckBar1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TrackBar trckBar1;
+    }
+}
